c9d7aa391ba541861771298134074741cf050071
mmgueye <117741127+mmgueye@users.noreply.github.com>
2024-03-28 15:33:16 +0100

    Delete ca_server/ca_server directory

--- ca_server/ca_server/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Certificate(models.Model):
-    user_id = models.IntegerField()
-    certificate_data = models.TextField()
-    status = models.CharField(max_length=20, default='pending')
--- ca_server/ca_server/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit-certificate-request/', views.submit_certificate_request, name='submit_certificate_request'),
-]
