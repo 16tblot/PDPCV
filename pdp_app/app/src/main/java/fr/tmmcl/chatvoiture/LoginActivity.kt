55fcffa86cd3bd27bc83aac048805d1dae65ab54
leofa <leo.favre@etu.u-bordeaux.fr>
2024-04-13 18:32:28 +0200

    feat:xml vue inside app

--- pdp_app/app/src/main/java/fr/tmmcl/chatvoiture/LoginActivity.kt
+++ pdp_app/app/src/main/java/fr/tmmcl/chatvoiture/LoginActivity.kt
@@ -9,9 +9,61 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import fr.tmmcl.chatvoiture.databinding.ActivityLoginBinding
 import fr.tmmcl.chatvoiture.databinding.ActivitySignupBinding
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+    private val client = OkHttpClient()
+    private val url = "https://chatvoiture.com/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //databaseHelper = DatabaseHelper(this)
+
+        binding.loginButton.setOnClickListener{
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = Hash256.hashPassword(binding.loginPassword.text.toString())
+            logindatabase(loginUsername, loginPassword)
+        }
+
+        binding.signupRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun logindatabase(username: String, password: String){
+        val getUrl = url + "?username=" + username +",password=" + password
+        val request = Request.Builder()
+            .url(getUrl)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val responseBody = response.body?.string()
+            if (responseBody.isNullOrEmpty()){
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+
+    /*
     private lateinit var binding: ActivityLoginBinding
     private lateinit var databaseHelper: DatabaseHelper
 
@@ -46,4 +98,5 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
         }
     }
+     */
 }
--- pdp_app/app/src/main/java/fr/tmmcl/chatvoiture/SignupActivity.kt
+++ pdp_app/app/src/main/java/fr/tmmcl/chatvoiture/SignupActivity.kt
@@ -9,10 +9,65 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import fr.tmmcl.chatvoiture.databinding.ActivityLoginBinding
 import fr.tmmcl.chatvoiture.databinding.ActivitySignupBinding
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import java.io.IOException
 
 class SignupActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivitySignupBinding
+    private val client = OkHttpClient()
+    private val url = "https://chatvoiture.com/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //databaseHelper = DatabaseHelper(this)
+
+        binding.signupButton.setOnClickListener{
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = Hash256.hashPassword(binding.signupPassword.text.toString())
+            signupdatabase(signupUsername, signupPassword)
+        }
+
+        binding.loginRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signupdatabase(username: String, password: String){
+        val requestBody = FormBody.Builder()
+            .add("username", username)
+            .add("password", password)
+            .build()
+
+        val request = Request.Builder().url(url).post(requestBody).build()
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val responseBody = response.body?.string()
+            if (responseBody.isNullOrEmpty()){
+                Toast.makeText(this, "Signup Successful", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Signup Failed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    /*
     private lateinit var binding: ActivitySignupBinding
     private lateinit var databaseHelper: DatabaseHelper
 
@@ -25,7 +80,7 @@ class SignupActivity : AppCompatActivity() {
 
         binding.signupButton.setOnClickListener{
             val signupUsername = binding.signupUsername.text.toString()
-            val signupPassword = binding.signupUsername.text.toString()
+            val signupPassword = Hash256.hashPassword(binding.signupUsername.text.toString())
             signupdatabase(signupUsername, signupPassword)
         }
 
@@ -46,5 +101,5 @@ class SignupActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Signup Failed", Toast.LENGTH_LONG).show()
         }
-    }
+    }*/
 }
